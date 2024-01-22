5c20df28080f32bf193a5ff1493c50e6272f5761
Boris Gaidukevich <Borishahn@yandex.ru>
2024-01-22 21:11:14 +0300

    0.0.1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace binaryTree
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
